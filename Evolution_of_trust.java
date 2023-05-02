5868d18afc260a75272ad51439b06864b60043b8
kaufmha <kaufmha@whs.school.nz>
2023-05-02 14:59:15 +1200

    Default files
    Signed-off-by: kaufmha <kaufmha@whs.school.nz>

---
+++ Evolution_of_trust.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Evolution_of_trust here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Evolution_of_trust
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Evolution_of_trust
+     */
+    public Evolution_of_trust()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
