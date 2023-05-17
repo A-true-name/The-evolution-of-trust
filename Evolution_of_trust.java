bc2fbaac496e82699bfd5b80089ff45286111f1b
kaufmha <kaufmha@whs.school.nz>
2023-05-18 11:35:07 +1200

    Signed-off-by: kaufmha <kaufmha@whs.school.nz>

--- Evolution_of_trust.java
+++ Evolution_of_trust.java
@@ -9,22 +9,36 @@ import java.util.Scanner; //Allows for the use of scanners.
 public class Evolution_of_trust
 {
 
-
+    private int x;
 
 
     public Evolution_of_trust()
     {
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("Cooperate or cheat"); 
+        int x=keyboard.nextInt();
+        Scanner inputStream = new Scanner(System.in);
+        String s1,s2,s3;
+        s1="Cooperate";
+        s2="Cheat";
+        s3="Random";
         int [] score = new int [20]; //Stores the score as a round goes on with each value in an array being the score for that round.
         score[0]=0;
         score[1]=0;
         score[2]=0;
         
-
-        
-        Scanner inputStream = new Scanner(System.in);
-        String s1;
-        
+ 
+      
+                switch (x){
+            case 1:System.out.println(s1);
+            break;
+            case 2:System.out.println(s2);
+            break;
+            case 4:System.out.println("Can't choose?");
+            default :System.out.println(s3);
+            //break;
+        }
+        //if (
 
     }
 }
